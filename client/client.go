ffa033f72689c52ca77f5a689628e8224df50c56
Fredrik Blomqvist <scanmar333@gmail.com>
2024-05-16 00:24:12 +0200

    add dynamic articles

--- client/client.go
+++ client/client.go
@@ -2,16 +2,27 @@ package client
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
-// Generate the html for the home page
-func Home(c *gin.Context) {
-	c.HTML(http.StatusOK, "index.tmpl", gin.H{
-		"title":   "Main wow",
-		"content": "home.tmpl",
-		// Specific for home.tmpl ↓
-		"text": "jag älskar faktiskt kth",
-	})
+// Home page
+func Home(text string) func(c *gin.Context) {
+	type Article struct {
+		Name       string
+		Date       time.Time
+		Coverimage string
+	}
+
+	articles := []Article{{"loldbuggen", time.Now(), ""}, {"hejdbuggen", time.Now().Add(30), "bild.png"}}
+
+	return func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title":   "Main wow",
+			"content": "home.tmpl",
+			// Specific for home.tmpl ↓
+			"articles": articles,
+		})
+	}
 }
--- server/server.go
+++ server/server.go
@@ -12,11 +12,11 @@ import (
 func Start() {
 	r := gin.Default()
 
-	tmpl := template.Must(template.ParseGlob("client/*.tmpl"))
+	tmpl := template.Must(template.ParseGlob("client/html/*.tmpl"))
 	r.Static("/css", "client/css")
 	r.Static("assets", "assets")
 
-	r.GET("/", client.Home)
+	r.GET("/", client.Home("lol vad sjuktr"))
 
 	r.SetHTMLTemplate(tmpl)
 	r.Run()
