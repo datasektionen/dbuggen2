c1b73d8a1f248da09f60479bfdb026221d929163
Fredrik Blomqvist <frblo@kth.se>
2024-11-29 22:10:07 +0100

    do shit

--- server/update_handler.go
+++ server/update_handler.go
@@ -1,16 +1,30 @@
 package server
 
 import (
-	"net/http"
+	"log"
+	"strconv"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jmoiron/sqlx"
 )
 
+func determineOrder(str string) []int {
+	orderStrings := strings.Split(str, ",")
+	var order []int
+	for i := 0; i < len(orderStrings); i++ {
+		index, err := strconv.Atoi(orderStrings[i])
+		if err != nil {
+			continue
+		}
+		order = append(order, index)
+	}
+	return order
+}
+
 func saveIssue(db *sqlx.DB) func(c *gin.Context) {
 	return func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "Fuck yeah",
-		})
+		order := determineOrder(c.PostForm("order-input"))
+		log.Print(order)
 	}
 }
